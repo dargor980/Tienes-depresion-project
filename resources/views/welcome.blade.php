144b80cdb1445c348593842101515ae8b93baf61
Germán Contreras <german.contrerasa@utem.cl>
2020-10-21 01:55:43 -0300

    avances en vista principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,81 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header class="align-self-center my-5">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 text-center">
+                <img src="/img/Inicio_mesa_de_trabajo.png" alt="" id="logo">
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-light nav justify-content-center">
+        <div class="justify-content-start">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse justify-content-center" id="lala">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-2">
+                    <button class="btn px-5" style="background-color: #455819; border-radius:10px;" id="opcion"><a href="" class="nav-link text-white bold" style="font-size: 18pt">Inicio</a></button>
+                </li>
+                <li class="nav-item mx-2">
+                    <button class="btn px-5" style="background-color: #455819; border-radius:10px;"><a href="" class="nav-link text-white bold" style="font-size: 18pt">Proyecto</a></button>
+                    
+                </li>
+                <li class="nav-item mx-2">
+                    <button class="btn px-5" style="background-color: #455819; border-radius:10px;"><a href="" class="nav-link text-white bold" style="font-size: 18pt">Test</a></button>         
+                </li>
+                <li class="nav-item mx-2">
+                    <button class="btn px-5" style="background-color: #455819; border-radius:10px;"><a href="" class="nav-link text-white bold" style="font-size: 18pt">Galería</a></button>             
+                </li>
+                <li class="nav-item mx-2">
+                    <button class="btn px-5" style="background-color: #455819; border-radius:10px;"><a href="" class="nav-link text-white bold" style="font-size: 18pt">Contacto</a></button>      
+                </li>
+            </ul>
+        </div> 
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <div class="auto" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">Inicio</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Proyecto</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Test</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Galería</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Contacto</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container text-center my-5">
+        <img src="/img/Inicio-13.png" alt="" style="" id="banner">
+    </div>
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-md-6">
+                <img src="/img/Inicio-02.png" alt="" style="width:415px; heigth:180px">
+            </div>
+            <div class="col-md-6">
+                <img src="/img/Inicio-03.png" alt="" style="width:570px; heigth:190px">
             </div>
         </div>
-    </body>
+        <div class="row my-3">
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
 </html>
